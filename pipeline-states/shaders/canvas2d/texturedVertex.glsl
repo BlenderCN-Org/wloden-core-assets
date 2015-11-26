732db65ca3d9dbbac48aac08b8d45f95beebf17d
Ronie Salgado <roniesalg@gmail.com>
2015-11-26 02:49:51 -0300

    I added more shaders.

--- pipeline-states/shaders/canvas2d/texturedVertex.glsl
+++ pipeline-states/shaders/canvas2d/texturedVertex.glsl
@@ -7,12 +7,15 @@ layout(std140, binding=0) uniform CanvasViewport
 };
 
 layout(location=0) in vec3 vPosition;
+layout(location=1) in vec2 vTexcoord;
 layout(location=2) in vec4 vColor;
 
+out vec2 fTexcoord;
 out vec4 fColor;
 
 void main()
 {
+    fTexcoord = vTexcoord;
     fColor = vColor;
     gl_Position = projectionMatrix * viewMatrix * vec4(vPosition, 1.0);
 }
--- pipeline-states/shaders/canvas2d/texturedVertex.hlsl
+++ pipeline-states/shaders/canvas2d/texturedVertex.hlsl
@@ -7,12 +7,14 @@ cbuffer CanvasViewport : register(b0)
 struct VertexInput
 {
     float4 position : A;
+    float2 texcoord : B;
     float4 color : C;
 };
 
 struct VertexOutput
 {
     float4 color : COLOR;
+    float2 texcoord : TEXCOORD;
     float4 position : SV_POSITION;
 };
 
@@ -20,6 +22,7 @@ VertexOutput main(VertexInput input)
 {
     VertexOutput output;
     output.color = input.color;
+    output.texcoord = input.texcoord;
     output.position = mul(projectionMatrix, mul(viewMatrix, input.position));
     return output;
 }
